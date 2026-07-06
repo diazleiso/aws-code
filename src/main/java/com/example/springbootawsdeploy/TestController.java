c47ac18412dbd6ff47f20943c0a2ccce20e50f2b
diazleiso <diazleiso@gmail.com>
2026-07-06 18:30:34 -0400

    update proyecto nuevo#3

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS Ecs sii";
+        return "First message from AWS Ecs sii cr77777";
     }
 
     @GetMapping("/message")
